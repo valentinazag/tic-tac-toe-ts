0418fa1dcfceb99f1df507fd7e7710e76c6b7aac
valentinazag <valentinazagman@gmail.com>
2025-10-29 18:09:17 +0000

    types of square and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const WINNING_LINES: number[][] = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type Move = "X" | "O";
+type Square = Move | null;
+
+function getWinner(squares: Square[]) {
+	for (let i = 0; i < WINNING_LINES.length; i++) {
+		const [a, b, c] = WINNING_LINES[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+}
+//I do this because every component that recienve prps those ned to have a type
+type SquareProps = {
+	clickSquare: () => void; //empty function
+	value: Square; // "X" | "O"
+};
+
+function Square({ clickSquare, value }: SquareProps) {
+	return (
+		<button type="button" className="square" onClick={clickSquare}>
+			{value}
+		</button>
+	);
 }
 
-export default App
+function Board() {
+	const [xIsNext, setxIsNext] = useState<boolean>(true);
+	const [squares, setSquares] = useState<Square[]>(new Array(9).fill(null));
+
+	function handleClick(i: number) {
+		if (getWinner(squares) || squares[i]) {
+			return;
+		}
+		const nextSquares = squares.slice();
+		if (xIsNext) {
+			nextSquares[i] = "X";
+		} else {
+			nextSquares[i] = "O";
+		}
+		setSquares(nextSquares);
+		setxIsNext(!xIsNext);
+	}
+
+	const winner = getWinner(squares);
+	let status: string;
+	if (winner) {
+		status = `the winner is ${winner}`;
+	} else {
+		status = `turn of ${xIsNext ? "X" : "O"}`;
+	}
+
+	function composeRows(squares: Square[]) {
+		const SIZE = 3;
+		const slices: Square[][] = [
+			squares.slice(0, SIZE),
+			squares.slice(SIZE, 2 * SIZE),
+			squares.slice(2 * SIZE, 3 * SIZE),
+		];
+		return slices;
+	}
+
+	const rows = composeRows(squares);
+
+	return (
+		<div className="rows">
+			{rows.map((row, rowInd) => {
+				const key = `square${rowInd}`;
+				return (
+					<div key={key}>
+						{row.map((_, column) => {
+							const square = rowInd * 3 + column;
+							return (
+								<Square
+									key={square}
+									value={squares[square]}
+									clickSquare={() => handleClick(square)}
+								/>
+							);
+						})}
+					</div>
+				);
+			})}
+			{status}
+		</div>
+	);
+}
+
+export function App() {
+	return (
+		<div>
+			<Board />
+		</div>
+	);
+}
+
+export default App;
